cc42531c58dd252ed6f71249a3dcedd6cc947111
Bruce Kroeze <bkroeze@gmail.com>
2009-07-30 08:19:47 -0700

    closes #853

---
+++ satchmo/apps/payment/modules/google/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+from livesettings import config_get_group, config_value
+
+config = config_get_group('PAYMENT_GOOGLE')
+
+urlpatterns = patterns('',
+     (r'^$', 'payment.modules.google.views.pay_ship_info', {'SSL': config.SSL.value}, 'GOOGLE_satchmo_checkout-step2'),
+     (r'^confirm/$', 'payment.modules.google.views.confirm_info', {'SSL': config.SSL.value}, 'GOOGLE_satchmo_checkout-step3'),
+     (r'^success/$', 'payment.views.checkout.success', {'SSL': config.SSL.value}, 'GOOGLE_satchmo_checkout-success'),
+     (r'^confirmorder/$', 'payment.views.confirm.confirm_free_order', 
+        {'SSL' : config.SSL.value, 'key' : 'GOOGLE'}, 'GOOGLE_satchmo_checkout_free-confirm')
+)
