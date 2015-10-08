ec8dc4dcc07bed1f86fbf7978f3e0df96d84890f
Grant Harding <gharding@gmail.com>
2015-10-08 12:16:14 -0400

    updated for livesettings

---
+++ satchmo/apps/satchmo_ext/productratings/__init__.py
@@ -0,0 +1,11 @@
+try:
+    from django.contrib.comments.models import Comment
+    from django.contrib.comments.signals import comment_will_be_posted, comment_was_posted
+except ImportError:
+    from django_comments.models import Comment
+    from django_comments.signals import comment_will_be_posted, comment_was_posted
+from listeners import *
+
+comment_was_posted.connect(save_rating, sender=Comment)
+comment_was_posted.connect(one_rating_per_product, sender=Comment)
+comment_was_posted.connect(check_with_akismet, sender=Comment)
---
+++ satchmo/apps/satchmo_ext/productratings/models.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+if 'django_comments' in settings.INSTALLED_APPS:
+    from django_comments.models import Comment    
+else:
+    from django.contrib.comments.models import Comment
+from satchmo_utils.signals import collect_urls
+import product
+import satchmo_store
+
+class ProductRating(models.Model):
+    """A rating attached to a comment"""
+    comment = models.OneToOneField(Comment, verbose_name="Rating", primary_key=True)
+    rating = models.IntegerField(_("Rating"))
+
+from urls import add_product_urls, add_comment_urls
+collect_urls.connect(add_product_urls, sender=product)
+collect_urls.connect(add_comment_urls, sender=satchmo_store)
